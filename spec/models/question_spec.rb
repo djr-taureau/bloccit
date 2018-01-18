9247f7efef5bdf664a7bf723ce830732af57afed
David J Ruyle <djr.taureau@gmail.com>
2018-01-18 09:15:34 -0600

    completed assignment-14

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe Question, type: :model do
 
   describe "attribute" do
     it "has title and body attributes" do
-      exp
+      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body")
+    end
+  end
+
 end
