09d3124091aea0ba7d22b255941fb96b6a56f9d1
David J Ruyle <djr.taureau@gmail.com>
2018-01-17 18:14:59 -0600

    completed assignment-12

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get 'welcome/contact'
 
+  get 'welcome/faq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
