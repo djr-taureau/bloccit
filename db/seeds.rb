cb75a3e7d8e67b074d8249cfebd6a44cc93dd7a2
David J Ruyle <djr.taureau@gmail.com>
2018-01-19 08:17:48 -0600

    completd assignment-18

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,17 @@ posts = Post.all
   )
 end
 
+100.times do 
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph,
+    price: Random.rand(11)
+  )
+end
+
 Post.find_or_create_by(title: "Dave has a title", body: "and a very unique body")
-if post.last.title = 
-puts "Seed finished"
+
 
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} ads created"
