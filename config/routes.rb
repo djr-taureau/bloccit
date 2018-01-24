efe4324fa7a8467fb0e1adee7ca72b3acd26b7eb
David J Ruyle <djr.taureau@gmail.com>
2018-01-24 13:30:08 -0600

    completed assignment-24

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
   
   root 'welcome#index'
+
+  post 'users/confirm' => 'users#confirm'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
