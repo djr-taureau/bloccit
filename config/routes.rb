0b6f185c14edff9bc80b4a8451ae98fedb7c86af
David J Ruyle <djr.taureau@gmail.com>
2018-01-27 15:19:03 -0600

    completed checkpoint-28

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   resources :topics do
     resources :posts, except: [:index]
-    resources :sponsored_posts, except: [:index]
+  end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
   end
 
   resources :advertisements
