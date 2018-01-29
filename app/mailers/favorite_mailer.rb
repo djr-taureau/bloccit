ec6a87792cd5c887090f2a3990ffa8843d4393e1
David J Ruyle <djr.taureau@gmail.com>
2018-01-29 15:59:16 -0600

    completed assignment-30

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,9 +1,4 @@
 class FavoriteMailer < ApplicationMailer
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> checkpoint-30
   def new_comment (user, post, comment)
     headers["Message-ID"] = "<comments/#{comment.id}@dry-brushlands-84617.herokuapp.com>"
     headers["In-Reply-To"] = "<post/#{post.id}@dry-brushlands-84617.herokuapp.com>"
@@ -14,5 +9,15 @@ class FavoriteMailer < ApplicationMailer
     @comment = comment
 
     mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+  def new_post(post)
+    headers["Message-ID"] = "<post/#{post.id}@dry-brushlands-84617.herokuapp.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@dry-brushlands-84617.herokuapp.com>"
+    headers["References"] = "<post/#{post.id}@dry-brushlands-84617.herokuapp.com>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You're following #{post.title}")
   end
 end
